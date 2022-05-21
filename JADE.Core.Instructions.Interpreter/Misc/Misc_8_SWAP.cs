63a77bc2cb21b6367c408cf7aa974db6a5cbdb3b
Benjamin <3627916+BLNJ@users.noreply.github.com>
2022-05-21 23:25:17 +0200

    Add 8-Bit SWAP Instructions

---
+++ JADE.Core.Instructions.Interpreter/Misc/Misc_8_SWAP.cs
@@ -0,0 +1,61 @@
+﻿using JADE.Core.Instructions.Bridge;
+using JADE.Core.Instructions.Bridge.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JADE.Core.Instructions.Interpreter.Misc
+{
+    public class Misc_8_SWAP
+    {
+        [Instruction(0x30, "SWAP B", isExtendedInstruction: true)]
+        [Instruction(0x31, "SWAP C", isExtendedInstruction: true)]
+        [Instruction(0x32, "SWAP D", isExtendedInstruction: true)]
+        [Instruction(0x33, "SWAP E", isExtendedInstruction: true)]
+        [Instruction(0x34, "SWAP H", isExtendedInstruction: true)]
+        [Instruction(0x35, "SWAP L", isExtendedInstruction: true)]
+        [Instruction(0x36, "SWAP (HL)", isExtendedInstruction: true)]
+        [Instruction(0x37, "SWAP A", isExtendedInstruction: true)]
+        public class SWAP_n : IInstruction
+        {
+            public bool PrepareParameters(byte opCode, ref List<InstructionParameterRequestBase> parametersList)
+            {
+                ParameterRegister sourceRegister = Helpers.BitHelper.OpCodeLowerNibbleToRegister(opCode);
+
+                if (sourceRegister == ParameterRegister.HL)
+                {
+                    parametersList.AddRelativeMemory(Bridge.Memory.ParameterRequestType.UnsignedByte, ParameterRegister.HL);
+                }
+                else
+                {
+                    parametersList.AddRegister(sourceRegister);
+                }
+
+                return true;
+            }
+
+            public byte Process(byte opCode, ref List<InstructionParameterResponseBase> parametersList, ref List<InstructionParameterResponseBase> changesList)
+            {
+                ParameterRegister sourceRegister = Helpers.BitHelper.OpCodeLowerNibbleToRegister(opCode);
+                byte value = (byte)parametersList[0].Value;
+
+                RegisterCommit registerCommit = new RegisterCommit();
+                byte valueSwap = InstructionMethods.Swap(registerCommit, value);
+
+                changesList.AddRegisterCommit(registerCommit);
+                if (sourceRegister == ParameterRegister.HL)
+                {
+                    changesList.AddRelativeMemory(Bridge.Memory.ParameterRequestType.UnsignedByte, ParameterRegister.HL, valueSwap);
+                    return 16;
+                }
+                else
+                {
+                    changesList.AddRegister(sourceRegister, valueSwap);
+                    return 8;
+                }
+            }
+        }
+    }
+}
