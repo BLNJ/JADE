5d6fb3703121f289a9c2e335db4228bfec1dab55
Benjamin <3627916+BLNJ@users.noreply.github.com>
2022-05-21 22:18:06 +0200

    Implement 8-Bit Load Decrease HL

---
+++ JADE.Core.Instructions.Interpreter/Load/Load_8_LDD.cs
@@ -0,0 +1,54 @@
+﻿using JADE.Core.Instructions.Bridge;
+using JADE.Core.Instructions.Bridge.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JADE.Core.Instructions.Interpreter.Load
+{
+    public static class Load_8_LDD
+    {
+        [Instruction(0x32, "LDD (HL), A")]
+        [Instruction(0x3A, "LDD A, (HL)")]
+        public class nn_nn : IInstruction
+        {
+            public bool PrepareParameters(byte opCode, ref List<InstructionParameterRequestBase> parametersList)
+            {
+                if(opCode == 0x32)
+                {
+                    parametersList.AddRegister(ParameterRegister.A);
+                }
+                else
+                {
+                    parametersList.AddRelativeMemory(Bridge.Memory.ParameterRequestType.UnsignedByte, ParameterRegister.HL);
+                }
+
+                parametersList.AddRegister(ParameterRegister.HL);
+
+                return true;
+            }
+
+            public byte Process(byte opCode, ref List<InstructionParameterResponseBase> parametersList, ref List<InstructionParameterResponseBase> changesList)
+            {
+                byte value = (byte)parametersList[0].Value;
+                ushort registerHL = (ushort)parametersList[1].Value;
+
+                if (opCode == 0x32)
+                {
+                    changesList.AddRelativeMemory(Bridge.Memory.ParameterRequestType.UnsignedByte, ParameterRegister.HL, value);
+                }
+                else
+                {
+                    changesList.AddRegister(ParameterRegister.A, value);
+                }
+
+                registerHL -= 1;
+                changesList.AddRegister(ParameterRegister.HL, registerHL);
+
+                return 8;
+            }
+        }
+    }
+}
