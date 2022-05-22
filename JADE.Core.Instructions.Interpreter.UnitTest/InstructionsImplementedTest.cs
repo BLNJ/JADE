8b9c6d7c87ed117e55d15aacca3dbcc98c245916
Benjamin <3627916+BLNJ@users.noreply.github.com>
2022-05-22 05:40:12 +0200

    Add UnitTest to check if all Instructions are implemented (spoiler: they´re not, I missed some)

---
+++ JADE.Core.Instructions.Interpreter.UnitTest/InstructionsImplementedTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JADE.Core.Instructions.Interpreter.UnitTest
+{
+    public class InstructionsImplementedTest
+    {
+        List<JADE.Core.Instructions.Bridge.InstructionAttribute> instructionAttributes = new List<JADE.Core.Instructions.Bridge.InstructionAttribute>();
+
+        [SetUp]
+        public void Setup()
+        {
+            instructionAttributes.Clear();
+
+            Type instructionInterfaceType = typeof(JADE.Core.Instructions.Bridge.IInstruction);
+            Type instructionAttributeType = typeof(JADE.Core.Instructions.Bridge.InstructionAttribute);
+
+            Type bla = typeof(JADE.Core.Instructions.Interpreter.BIT_8_BIT); //if I dont do that we cant "see" the Assembly
+
+            AppDomain currentAppDomain = AppDomain.CurrentDomain;
+            System.Reflection.Assembly[] assemblies = currentAppDomain.GetAssemblies();
+
+            for (int i = 0; i < assemblies.Length; i++)
+            {
+                System.Reflection.Assembly assembly = assemblies[i];
+
+                Type[] assemblyTypes = assembly.GetTypes();
+                for (int j = 0; j < assemblyTypes.Length; j++)
+                {
+                    Type assemblyType = assemblyTypes[j];
+
+                    if (instructionInterfaceType.IsAssignableFrom(assemblyType) &&
+                        !assemblyType.IsAbstract)
+                    {
+                        object[] attributes = assemblyType.GetCustomAttributes(false);
+                        for (int k = 0; k < attributes.Length; k++)
+                        {
+                            object attribute = attributes[k];
+
+                            if (attribute.GetType() == instructionAttributeType)
+                            {
+                                JADE.Core.Instructions.Bridge.InstructionAttribute instructionAttribute = (JADE.Core.Instructions.Bridge.InstructionAttribute)attribute;
+                                instructionAttributes.Add(instructionAttribute);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [Test]
+        public void CheckAllInstructionsExistTest()
+        {
+            int[] missingOpCodes = new int[]
+            {
+                0xD3, 0xDB, 0xDD,
+                0xE3, 0xE4, 0xEB, 0xEC, 0xED,
+                0xF4, 0xFC, 0xFD
+            };
+
+            List<(bool, int)> unimplementedInstruction = new List<(bool, int)>();
+
+            bool isExtended = false;
+            for(int passes = 0; passes < 2; passes++)
+            {
+                for(int opCode = 0; opCode <= 0xFF; opCode++)
+                {
+                    if(!isExtended)
+                    {
+                        if(missingOpCodes.Contains(opCode))
+                        {
+                            continue;
+                        }
+                    }
+
+                    JADE.Core.Instructions.Bridge.InstructionAttribute? instructionAttribute = this.instructionAttributes.Find(ins => ins.IsExtendedInstruction == isExtended && ins.OpCode == opCode);
+
+                    if(instructionAttribute == null)
+                    {
+                        unimplementedInstruction.Add((isExtended, opCode));
+                    }
+                }
+
+                isExtended = true;
+            }
+
+            if(unimplementedInstruction.Count > 0)
+            {
+                string message = String.Format("{0} Missing: ", unimplementedInstruction.Count);
+                for(int i = 0; i < unimplementedInstruction.Count; i++)
+                {
+                    (bool, int) instruction = unimplementedInstruction[i];
+                    message += string.Format("[{0}, {1}], {2}", instruction.Item1, instruction.Item2.ToString("X2"), Environment.NewLine);
+                }
+
+                Assert.Fail(message);
+            }
+            else
+            {
+                Assert.Pass();
+            }
+        }
+    }
+}
