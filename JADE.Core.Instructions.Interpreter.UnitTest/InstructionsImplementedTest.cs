90eb54b3cf0df7316b95c5ad71b0438ca0fbcc54
Benjamin <3627916+BLNJ@users.noreply.github.com>
2022-05-23 17:40:17 +0200

    Add 0xCB to ignored opCodes for obvious reasons

--- JADE.Core.Instructions.Interpreter.UnitTest/InstructionsImplementedTest.cs
+++ JADE.Core.Instructions.Interpreter.UnitTest/InstructionsImplementedTest.cs
@@ -56,6 +56,7 @@ namespace JADE.Core.Instructions.Interpreter.UnitTest
         {
             int[] missingOpCodes = new int[]
             {
+                0xCB,
                 0xD3, 0xDB, 0xDD,
                 0xE3, 0xE4, 0xEB, 0xEC, 0xED,
                 0xF4, 0xFC, 0xFD
