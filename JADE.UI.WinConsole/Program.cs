218f6ac87674fee89743085c541b25542ba0999a
Benjamin <3627916+BLNJ@users.noreply.github.com>
2022-05-24 02:43:07 +0200

    Set up WinConsole to test functions

--- JADE.UI.WinConsole/Program.cs
+++ JADE.UI.WinConsole/Program.cs
@@ -6,7 +6,16 @@ namespace JADE.UI.WinConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string romPath = "C:\\Gameboy_Dev\\totallyLegitTetris.gb";
+
+            JADE.Core.Device device = new Core.Device();
+            device.Reset();
+
+            JADE.Core.ReadOnlyMemory.ROM rom = new Core.ReadOnlyMemory.ROM(device);
+            rom.OpenFile(romPath);
+
+            device.InsertROM(rom);
+            device.Start();
         }
     }
 }
