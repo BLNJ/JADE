e11a8615768bd7b2f4a1237909a534d57de88765
Benjamin <3627916+BLNJ@users.noreply.github.com>
2022-06-01 08:03:47 +0200

    Added (new?) and working .NET WPF Frontend for testing

---
+++ JADE.UI.WinWPF/Windows/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JADE.UI.WinWPF.Windows
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        JADE.Core.Device device;
+        JADE.Core.ReadOnlyMemory.ROM rom;
+
+        public MainWindow()
+        {
+            this.device = new Core.Device();
+            this.device.PPU.PictureDrawn += ppu_PictureDrawn;
+
+            InitializeComponent();
+            this.DataContext = this.device;
+        }
+
+        private void ppu_PictureDrawn(object sender, System.Drawing.Bitmap image)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnExecute_Click(object sender, RoutedEventArgs e)
+        {
+            string romPath = "C:\\Gameboy_Dev\\totallyLegitTetris.gb";
+            rom = new Core.ReadOnlyMemory.ROM(device);
+            rom.OpenFile(romPath);
+
+            device.InsertROM(rom);
+            device.Start();
+        }
+    }
+}
