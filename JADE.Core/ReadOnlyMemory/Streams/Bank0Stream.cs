620f6d091bdab495f9b640eed8985aa3db8f2581
Benjamin <3627916+BLNJ@users.noreply.github.com>
2022-06-12 08:46:26 +0200

    Add Stream for Bank0 of ROM, because some commercial ROM´s like to write to it for some fucked up reason, although it does nothing. A NOP would be better, but eh, who am I do judge

---
+++ JADE.Core/ReadOnlyMemory/Streams/Bank0Stream.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JADE.Core.ReadOnlyMemory.Streams
+{
+    public class Bank0Stream : JADE.IO.ExternalMemory
+    {
+        public Bank0Stream(Stream baseStream) : base(baseStream, true)
+        {
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            //This is a Bank0 Stream, writing should be done
+            //But if something has that stupid idea, we will just ignore it
+            //Just like our Sponsor, Freshdesk
+        }
+    }
+}
